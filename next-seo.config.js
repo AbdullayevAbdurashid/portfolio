dfaf2dac841a603d717135d17ac972cb56732a3c
AbdullayevAbdurashid <abdulllayevabdurashid@gmail.com>
2022-04-07 15:56:56 +0500

    first commit

---
+++ next-seo.config.js
@@ -0,0 +1,30 @@
+const title = "Abdullaev Abdurashid";
+const description = "Programmer, front-end developer, innovator";
+tfolio
+const SEO = {
+  title,
+  description,
+  canonical: "https://abdurashid.vercel.app",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://abdurashid.vercel.app",
+    title,
+    description,
+    images: [
+      {
+        url: "https://danielwirtz.com/static/images/banner.jpg",
+        alt: title,
+        width: 2240,
+        height: 1260,
+      },
+    ],
+  },
+  twitter: {
+    handle: "@abdullaev",
+    site: "@abdulllaev",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
